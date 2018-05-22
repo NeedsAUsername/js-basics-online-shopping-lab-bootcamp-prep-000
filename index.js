df04c7a1372c35e8470b7da54dc9ad13ec6fb690
Kevin Chi <kevinchi118@gmail.com>
2018-05-22 22:58:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice([i],1)
     }
-    
+  if (cart.length === length) {
+    return 'That item is not in your cart'
+  }   
   }
 }
 
