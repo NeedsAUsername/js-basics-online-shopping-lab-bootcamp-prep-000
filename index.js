2fba3b3f9dc1c636818173a98b5e6cd41778ecda
Kevin Chi <kevinchi118@gmail.com>
2018-05-22 23:11:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice([i],1)
       counter += 1
-      return cart
     }
   if (counter === 0) {
     return 'That item is not in your cart.'
   }   
+  return cart
   }
 }
 
@@ -67,5 +67,5 @@ function placeOrder(cardNumber) {
 }
 
 addToCart('Apple')
-console.log(removeFromCart('Aple'))
+console.log(removeFromCart('Apple'))
 console.log(cart)
