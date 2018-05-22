d93f27c65869192fb34437b4f7c19ebc9d0dfd82
Kevin Chi <kevinchi118@gmail.com>
2018-05-22 23:13:48 +0000

    automatically backed up by learn

--- notes.js
+++ notes.js
@@ -8,5 +8,5 @@ math.random() returns random number between 0-1
 a = [{itemName: 'apple', itemPrice: 10}, {itemName: 'orange', itemPrice:5}]
 
 console.log(a)
-a.splice(0)
+a.splice(0, 1)
 console.log(a)
