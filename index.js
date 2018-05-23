bfed45152e96f13b70d862acd4a88ca72c8ec01c
Kevin Chi <kevinchi118@gmail.com>
2018-05-23 02:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,71 +14,66 @@ function addToCart(item) {
    itemName: item,
    itemPrice: (Math.floor(Math.random() * 99)+1)
  };
- cart.push(newObj)
- return `${item} has been added to your cart.`
+ cart.push(newObj);
+ return `${item} has been added to your cart.`;
 }
 
 
 function viewCart() {
-  var a = 'In your cart, you have '
+  var a = 'In your cart, you have ';
   if (cart.length === 0) {
-    return "Your shopping cart is empty."
+    return "Your shopping cart is empty.";
   }
   else {
     for (var i = 0; i < cart.length; i+=1) {
       if (cart.length === 1) {
-        a+= `${cart[i].itemName} at $${cart[i].itemPrice}.`
+        a+= `${cart[i].itemName} at $${cart[i].itemPrice}.`;
       }
       else if (i+1 === cart.length) {
-        a+= `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+        a+= `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
       }
       else {
-      a+= `${cart[i].itemName} at $${cart[i].itemPrice}, `
+      a+= `${cart[i].itemName} at $${cart[i].itemPrice}, `;
     }
     }
-  return a
+  return a;
   }
 }
 
 function total() {
- var value = 0
+ var value = 0;
   for (var i = 0; i<cart.length; i+=1) {
-    value += cart[i].itemPrice
+    value += cart[i].itemPrice;
   }
-  return value
+  return value;
 }
 
 function removeFromCart(item) {
-  var checkCart = 'not in cart'
+  var checkCart = 'not in cart';
   for (var i = 0; i<cart.length; i+=1) {
     if (cart[i].itemName === item) {
-      cart.splice([i],1)
-      checkCart = 'removed'
+      cart.splice([i],1);
+      checkCart = 'removed';
     }
   }
   if (checkCart === 'not in cart') {
-    return 'That item is not in your cart.'
+    return 'That item is not in your cart.';
   }   
 }
 
 function clearOrder() {
-  cart = []
+  cart = [];
 }
 
 function placeOrder(cardNumber) {
-  var x = total()
-  clearOrder()
+  var x = total();
+  clearOrder();
     if (Number.isInteger(cardNumber) === true) {
-      return `Your total cost is $${x}, which will be charged to the card ${cardNumber}.`
+      return `Your total cost is $${x}, which will be charged to the card ${cardNumber}.`;
     }
     else {
-      return 'Sorry, we don\'t have a credit card on file for you.'
+      return 'Sorry, we don\'t have a credit card on file for you.';
     }
-  
 }
 
-console.log(cart)
-addToCart('apple')
-console.log(cart)
-console.log(placeOrder(955))
-console.log(cart)
+
